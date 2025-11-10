c53d3d2507b67f3899f80bea053569b927b2b476
Felps-26 <filipiramos4152@gmail.com>
2025-11-09 23:53:54 -0300

    .

--- PROJETO_2ºBI/main.c
+++ PROJETO_2ºBI/main.c
@@ -3,55 +3,27 @@
 #include <string.h>
 #include "cidades.h"
 
-int main () {       
+int main () {
+
     int Soma = 0;
     int NumTeste = 0;
+
     FILE *Resposta = fopen("Resultado.txt", "w");
     Estrada *T1 = getEstrada("Teste01.txt");
     double D1 = calcularMenorVizinhanca("Teste01.txt");
     char *C1 = cidadeMenorVizinhanca("Teste01.txt");
 
-    printf("\nTestando T1->T: Esperado = 10, Obtido = %d\n", T1->T);
-    if (T1->T == 10) {
-        printf("Teste T1->T passou!\n");
-        Soma++;
-    } else {
-        printf("Teste T1->T falhou!\n");
-    }
+    if (T1->T == 10) Soma++;
     NumTeste++;
 
-    printf("\nTestando T1->N: Esperado = 2, Obtido = %d\n", T1->N);
-    if (T1->N == 2) {
-        printf("Teste T1->N passou!\n");
-        Soma++;
-    } else {
-        printf("Teste T1->N falhou!\n");
-    }
+    if (T1->N == 2) Soma++;
     NumTeste++;
 
-    printf("\nTestando D1: Esperado = 3.5, Obtido = %.2f\n", D1);
-    if (D1 == 3.5) {
-        printf("Teste D1 passou!\n");
-        Soma++;
-    } else {
-        printf("Teste D1 falhou!\n");
-    }
+    if (D1 == 3.5) Soma++;
     NumTeste++;
 
-    printf("\nTestando C1: Esperado = 'Birnin Zana', Obtido = '%s'\n", C1);
-    if (strcmp(C1, "Birnin Zana")==0) {
-        printf("Teste C1 passou!\n");
-        Soma++;
-    } else {
-        printf("Teste C1 falhou!\n");
-    }
+    if (strcmp(C1, "Birnin Zana")==0) Soma++;
     NumTeste++;
-
-    fprintf(Resposta, "\n\nATENÇÃO: Você acertou %d de %d itens. Logo, em 1.50 pontos, sua nota foi %.2f.\n", 
-            Soma, NumTeste, 2.0 * (float)Soma/(float)NumTeste);
     
-    printf("\nResultado final: %d/%d testes passaram.\n", Soma, NumTeste);
-    
-    fclose(Resposta);
-    return 0;
+    fprintf(Resposta, "\n\nATENÇÃO: Você acertou %d de %d itens. Logo, em 1.50 pontos, sua nota foi %.2f.\n", Soma, NumTeste, 2.0 * (float)Soma/(float)NumTeste);
 }
